1f5b85dd01180404c52379c57b225ae9fbf301e5
astra <zorkin.roman@gmail.com>
2022-02-15 03:33:57 +0300

    start second exerxise

--- 2_if2.py
+++ 2_if2.py
@@ -16,23 +16,42 @@
 """
 from typing import Any
 
-def string_control(first_strig: Any, second_string: Any) -> int:
+
+def string_control(first_value: Any, second_value: Any) -> int:
     """The function checks the arguments whether they are strings.
 
     Args:
-        first_strig (Any): first arg for controkl
-        second_string (Any): second arg for control
+        first_value (Any): first arg for control
+        second_value (Any): second arg for control
 
     Returns:
         int: The result of the control in the form of a conditional digit
     """
+    if not isinstance(first_value, str) or not isinstance(second_value, str):
+        return 0
+    elif first_value == second_value:
+        return 1
+    else:
+        if len(first_value) > len(second_value):
+            return 2
+        elif second_value == 'learn':
+            return 3
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    control_phrase = [
+        (1, 'vasy'), ('pety', 'tany'), ('pety', 'pety'),
+        ('python1', 'bee'), ('bee', 'python2'), ('learn', 'python3'),
+        ('python', 'learn'), ('bee', 'learn'), (1, 2),
+    ]
 
-if __name__ == "__main__":
+    for first_value, second_value in control_phrase:
+        print(string_control(first_value, second_value))
+
+
+if __name__ == '__main__':
     main()
